6b58fdbcc899141fb7b8f348a60c1c085b6f5f8c
Ray <hongrui.deng@googlemail.com>
2020-10-27 17:57:22 +0100

    Workspace backup @27.10.2020

--- try_enum/src/main.rs
+++ try_enum/src/main.rs
@@ -12,7 +12,16 @@ fn main() {
     let loopback = IpAddr {
         kind: IpAddrKind::V6(String::from("fdkas")),
         address: String::from("::1"),
-    };
+    }; 
+    let some_number = Some(5); 
+    let some_string = Some("a string"); 
+    let absent_number: Option<i32> = None;
+
+    let x: i8 = 5;
+    let y: Option<i8> = Some(5);
+
+    //let sum = x + y;
+    value_in_cents(Coin::Quarter(UsState::Alaska));
 }
 
 enum IpAddrKind {
@@ -23,4 +32,32 @@ enum IpAddrKind {
 struct IpAddr {
     kind: IpAddrKind,
     address: String,
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        }
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
+    }
+}
+
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
 }
